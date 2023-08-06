33f73197ea245afa4371ec67cc54af184bf3312b
Zeb Piasecki <zeb@zebulon.dev>
2023-08-06 11:53:05 -0400

    add logging to function

--- functions/[[slug]].js
+++ functions/[[slug]].js
@@ -1,2 +1,8 @@
-export const onRequestGet = async ({ params }) =>
-  new Response(`Hit a function ${params.slug}!`);
+/**
+ * @param {{ request: Request, params: { slug: string }}}} param
+ * @returns
+ */
+export const onRequestGet = async ({ request, params }) => {
+  console.log(request.url);
+  return new Response(`Hit a function ${params.slug}!`);
+};
